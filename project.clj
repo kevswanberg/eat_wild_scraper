3317e49d510faef2440f634e33a9f737c9d059c7
Kevan Swanberg <kevswanberg@gmail.com>
2018-07-06 22:50:47 -0400

    WIP scraping the page for each farm

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject eat_wild "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "FIXME: Scrape eat wild website for farms and locations"
+  :main eat-wild.core
+  :dependencies [
+[org.clojure/clojure "1.8.0"]
+[enlive "1.1.6"]
+[http-kit "2.1.18"]
+])
--- src/eat_wild/core.clj
+++ src/eat_wild/core.clj
@@ -1,6 +1,34 @@
-(ns eat-wild.core)
+(ns eat-wild.core
+    (:require [net.cgrand.enlive-html :as html]
+              [org.httpkit.client :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-dom
+  []
+  (html/html-snippet
+      (:body @(http/get "http://www.eatwild.com/products/index.html" {:insecure? true}))))
+
+(defn extract-states
+    [dom]
+    (map
+        (comp :href :attrs) (html/select dom [:blockquote :a])))
+
+
+
+(defn get-farm-dom
+      [state]
+      (html/html-snippet(:body @(http/get (str "http://www.eatwild.com/products/" state) {:insecure? true}))))
+
+(defn extract-farms
+   [dom]
+   (map
+        (comp first :content) (html/select dom [:p.bodyMargin])))
+
+;; (defn -main
+;;     []
+;;     (let [states (extract-states (get-dom))]
+;;         (println states)))
+
+(defn -main
+      []
+      (let [paragraphs (extract-farms (get-farm-dom "alaska.html"))]
+      (println (first paragraphs))))
